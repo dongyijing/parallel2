664f14bd05711896d96631196b716e998383ab29
egg <18942236750@163.com>
2017-11-13 09:47:30 +0800

    first commit

---
+++ main.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+#include <math.h>
+#include "solve.h"
+
+int main(){
+    int n;
+    double delta_x;/**< Step length in x direction*/
+    double delta_y;/**< Step length in y direction*/
+    double a, b;
+    double E;
+
+    n = (2*N-1)*(N-1);
+    delta_x = 2*sqrt(3)/(3*N);
+    delta_y = sqrt(3)*delta_x;
+
+    a = 1/(delta_x*delta_x);
+    b = 1/(delta_y*delta_y); 
+
+    E = 0.0001;
+
+    jacobi(n, E, a, b);
+}
+
+
+   
